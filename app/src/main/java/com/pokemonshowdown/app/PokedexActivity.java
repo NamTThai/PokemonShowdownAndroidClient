7ad6c5fd86c1a5cdd74727e8c489324621728cec
NamTThai <Nam.T.Thai@espn.com>
2014-08-09 22:11:59 -0400

    get rid of single fragment activity

---
+++ app/src/main/java/com/pokemonshowdown/app/PokedexActivity.java
@@ -0,0 +1,42 @@
+package com.pokemonshowdown.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PokedexActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedex);
+        getActionBar().setTitle(R.string.bar_pokedex);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.clear();
+        getMenuInflater().inflate(R.menu.fragment_pokedex, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(this) != null)
+                    NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.menu_pokedex_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
