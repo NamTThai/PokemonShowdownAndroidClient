d6db4680698db5f4ef5a8948b0d5f06b9c18e464
NamTThai <nam.t.thai@espn.com>
2014-10-04 15:36:12 -0400

    a bit of battlelog

--- app/src/main/java/com/pokemonshowdown/app/BattleFragment.java
+++ app/src/main/java/com/pokemonshowdown/app/BattleFragment.java
@@ -3,6 +3,7 @@ package com.pokemonshowdown.app;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.effect.Effect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -19,16 +20,20 @@ import android.text.style.StyleSpan;
 import android.text.style.TypefaceSpan;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.pokemonshowdown.data.BattleFieldData;
+import com.pokemonshowdown.data.MyApplication;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -104,6 +109,23 @@ public class BattleFragment extends android.support.v4.app.Fragment {
                 mRoomId = getArguments().getString(ROOM_ID);
             }
 
+            final EditText chatBox = (EditText) view.findViewById(R.id.battle_chat_box);
+            chatBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_DONE) {
+                        String message = chatBox.getText().toString();
+                        message = mRoomId + "|" + message;
+                        if (MyApplication.getMyApplication().verifySignedInBeforeSendingMessage()) {
+                            MyApplication.getMyApplication().sendClientMessage(message);
+                        }
+                        chatBox.setText(null);
+                        return false;
+                    }
+                    return false;
+                }
+            });
+
             return view;
         }
 
@@ -140,9 +162,14 @@ public class BattleFragment extends android.support.v4.app.Fragment {
         }
 
         public void processServerMessage(String message) {
-            String command = (message.indexOf('|') == -1) ? message : message.substring(0, message.indexOf('|'));
             BattleFieldData.RoomData roomData = BattleFieldData.get(getActivity()).getRoomInstance(mRoomId);
+            String command = (message.indexOf('|') == -1) ? message : message.substring(0, message.indexOf('|'));
             final String messageDetails = message.substring(message.indexOf('|') + 1);
+            if (command.startsWith("-")) {
+                processMinorAction(command, messageDetails);
+                return;
+            }
+
             int separator = messageDetails.indexOf('|');
             int start;
             String remaining;
@@ -185,6 +212,7 @@ public class BattleFragment extends android.support.v4.app.Fragment {
                     break;
                 case "message":
                     appendServerMessage(new SpannableString(messageDetails));
+                    break;
                 case "gametype":
                 case "gen":
                     break;
@@ -273,7 +301,7 @@ public class BattleFragment extends android.support.v4.app.Fragment {
                     appendServerMessage(toAppendSpannable);
                     break;
                 case "move":
-                    String attacker = messageDetails.substring(4, separator);
+                    String attacker = messageDetails.substring(5, separator);
                     remaining = messageDetails.substring(separator + 1);
                     toAppendBuilder = new StringBuilder();
                     if (remaining.startsWith("p2")) {
@@ -287,18 +315,15 @@ public class BattleFragment extends android.support.v4.app.Fragment {
                     toAppendSpannable = new SpannableString(toAppend);
                     toAppendSpannable.setSpan(new StyleSpan(Typeface.BOLD), start, start + move.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                     appendServerMessage(toAppendSpannable);
-                    if (remaining.contains("[miss]")) {
-                        toAppendBuilder = new StringBuilder();
-                        toAppendBuilder.append(attacker).append(" missed the target!");
-                        toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
-                        toAppendSpannable.setSpan(new RelativeSizeSpan(0.75f), 0, toAppendBuilder.toString().length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        appendServerMessage(toAppendSpannable);
-                    }
                     break;
                 case "switch":
                 case "drag":
                     toAppendBuilder = new StringBuilder();
-                    attacker = messageDetails.substring(4, separator);
+                    attacker = messageDetails.substring(5, separator);
+                    remaining = messageDetails.substring(separator + 1);
+                    separator = remaining.indexOf(',');
+                    String species = remaining.substring(0, separator);
+                    attacker = (!attacker.equals(species)) ? attacker + " (" + species + ")" : attacker;
                     if (messageDetails.startsWith("p1")) {
                         toAppendBuilder.append("Go! ").append(attacker).append('!');
                     } else {
@@ -309,7 +334,7 @@ public class BattleFragment extends android.support.v4.app.Fragment {
                 case "detailschange":
                     break;
                 case "faint":
-                    attacker = messageDetails.substring(4);
+                    attacker = messageDetails.substring(5);
                     toAppendBuilder = new StringBuilder();
                     if (messageDetails.startsWith("p2")) {
                         toAppendBuilder.append("The opposing ");
@@ -326,9 +351,224 @@ public class BattleFragment extends android.support.v4.app.Fragment {
                     toAppendSpannable.setSpan(new ForegroundColorSpan(R.color.dark_blue), 0, toAppend.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                     appendServerMessage(toAppendSpannable);
                     break;
+                case "win":
+                    toAppend = messageDetails + " has won the battle!";
+                    appendServerMessage(new SpannableString(toAppend));
+                    break;
                 default:
                     appendServerMessage(new SpannableString(message));
             }
+        }
+
+        private void processMinorAction(String command, String messageDetails) {
+            if (messageDetails.contains("[silent]")) {
+                return;
+            }
+            int separator;
+            int start;
+            String remaining;
+            String toAppend = "";
+            StringBuilder toAppendBuilder = new StringBuilder();
+            Spannable toAppendSpannable = new SpannableString(toAppend);
+
+            String fromEffect;
+            String ofSource;
+            int from = messageDetails.indexOf("[from]");
+            if (from != -1) {
+                remaining = messageDetails.substring(from + 7);
+                separator = remaining.indexOf('|');
+                fromEffect = (separator == -1) ? remaining : remaining.substring(0, separator);
+            }
+            int of = messageDetails.indexOf("[of]");
+            if (of != -1) {
+                remaining = messageDetails.substring(of + 5);
+                separator = remaining.indexOf('|');
+                ofSource = (separator == -1) ? remaining : remaining.substring(0, separator);
+            }
+
+            separator = messageDetails.indexOf('|');
+            switch (command) {
+                case "message":
+                    toAppendSpannable = new SpannableString(messageDetails);
+                    break;
+                case "-miss":
+                    String attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    toAppendBuilder.append(" missed the target");
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    break;
+                case "-fail":
+                    attacker = messageDetails.substring(5, separator);
+                    String action = messageDetails.substring(separator + 1);
+                    toAppend = action + " failed against " + attacker;
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-damage":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppendBuilder.append(" has lost ").append(remaining);
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    /*
+                    separator = remaining.indexOf(" ");
+                    String hp = remaining.substring(0, separator);
+                    if (hp.equals("0")) {
+                        toAppendBuilder.append("has fainted!");
+                    }*/
+                    break;
+                case "-heal":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppendBuilder.append(" healed ").append(remaining);
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    /*
+                    separator = remaining.indexOf(" ");
+                    String hp = remaining.substring(0, separator);
+                    if (hp.equals("0")) {
+                        toAppendBuilder.append("has fainted!");
+                    }*/
+                    break;
+                case "-status":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppendBuilder.append(" was inflicted with ").append(remaining);
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    /*
+                    separator = remaining.indexOf(" ");
+                    String hp = remaining.substring(0, separator);
+                    if (hp.equals("0")) {
+                        toAppendBuilder.append("has fainted!");
+                    }*/
+                    break;
+                case "-curestatus":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppendBuilder.append(" was cured from ").append(remaining);
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    /*
+                    separator = remaining.indexOf(" ");
+                    String hp = remaining.substring(0, separator);
+                    if (hp.equals("0")) {
+                        toAppendBuilder.append("has fainted!");
+                    }*/
+                    break;
+                case "-cureteam":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    toAppendBuilder.append(" cured the whole team from bad status");
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    /*
+                    separator = remaining.indexOf(" ");
+                    String hp = remaining.substring(0, separator);
+                    if (hp.equals("0")) {
+                        toAppendBuilder.append("has fainted!");
+                    }*/
+                    break;
+                case "-boost":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppendBuilder.append(" has boosted ");
+                    separator = remaining.indexOf('|');
+                    String stat = remaining.substring(0, separator);
+                    String amount = remaining.substring(separator + 1);
+                    toAppendBuilder.append(stat).append(" by ").append(amount);
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    break;
+                case "-unboost":
+                    attacker = messageDetails.substring(5, separator);
+                    if (messageDetails.startsWith("p2")) {
+                        toAppendBuilder.append("The opposing ");
+                    }
+                    toAppendBuilder.append(attacker);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppendBuilder.append(" has reduced ");
+                    separator = remaining.indexOf('|');
+                    stat = remaining.substring(0, separator);
+                    amount = remaining.substring(separator + 1);
+                    toAppendBuilder.append(stat).append(" by ").append(amount);
+                    toAppendSpannable = new SpannableStringBuilder(toAppendBuilder);
+                    break;
+                case "-weather":
+                    toAppend = "Weather changed to " + messageDetails;
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-crit":
+                    toAppendSpannable = new SpannableString("It's a critical hit!");
+                    break;
+                case "-supereffective":
+                    toAppendSpannable = new SpannableString("It's super effective!");
+                    break;
+                case "-resisted":
+                    toAppendSpannable = new SpannableString("It's not very effective");
+                    break;
+                case "-immune":
+                    attacker = messageDetails.substring(5);
+                    toAppend = attacker + " is immuned";
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-item":
+                    attacker = messageDetails.substring(5, separator);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppend = attacker + " revealed its " + remaining;
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-enditem":
+                    attacker = messageDetails.substring(5, separator);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppend = attacker + " has lost its " + remaining;
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-ability":
+                    attacker = messageDetails.substring(5, separator);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppend = attacker + " revealed its ability " + remaining;
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-endability":
+                    attacker = messageDetails.substring(5);
+                    toAppend = attacker + " lost its ability";
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-transform":
+                    attacker = messageDetails.substring(5, separator);
+                    remaining = messageDetails.substring(separator + 1);
+                    toAppend = attacker + " transformed into " + remaining;
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                case "-activate":
+                    toAppend = messageDetails + " has activated";
+                    toAppendSpannable = new SpannableString(toAppend);
+                    break;
+                default:
+                    toAppendSpannable = new SpannableString(command + ":" + messageDetails);
+            }
+            toAppendSpannable.setSpan(new RelativeSizeSpan(0.8f), 0, toAppendSpannable.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            appendServerMessage(toAppendSpannable);
         }
 
         private void appendServerMessage(final Spannable message) {
