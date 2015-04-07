033320d5bfca378f5c75671e6ae1fb6df250805a
NamTThai <nam.t.thai@espn.com>
2015-04-07 14:52:19 -0400

    BroadcastSender

--- app/src/main/java/com/pokemonshowdown/application/MyApplication.java
+++ app/src/main/java/com/pokemonshowdown/application/MyApplication.java
@@ -2,10 +2,8 @@ package com.pokemonshowdown.application;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.pokemonshowdown.data.BattleFieldData;
@@ -29,23 +27,6 @@ import java.util.Iterator;
  */
 public class MyApplication extends Application {
     public final static String MTAG = MyApplication.class.getName();
-    public final static String ACTION_FROM_MY_APPLICATION = "From My Application";
-    public final static String EXTRA_DETAILS = "Details";
-    public final static String EXTRA_NO_INTERNET_CONNECTION = "No Internet Connection";
-    public final static String EXTRA_WATCH_BATTLE_LIST_READY = "Watch Battle List Ready";
-    public final static String EXTRA_AVAILABLE_FORMATS = "Available Formats";
-    public final static String EXTRA_NEW_BATTLE_ROOM = "New Room";
-    public final static String EXTRA_SERVER_MESSAGE = "New Server Message";
-    public final static String EXTRA_REQUIRE_SIGN_IN = "Require Sign In";
-    public final static String EXTRA_ERROR_MESSAGE = "Error Message";
-    public final static String EXTRA_UNKNOWN_ERROR = "Unknown Error";
-    public final static String EXTRA_UPDATE_SEARCH = "Search Update";
-    public final static String EXTRA_CHANNEL = "Channel";
-    public final static String EXTRA_ROOMID = "RoomId";
-    public final static String EXTRA_UPDATE_AVAILABLE = "Update Available";
-    public final static String EXTRA_SERVER_VERSION = "Server Version";
-    public final static String EXTRA_LOGIN_SUCCESSFUL = "Login Successful";
-    public final static String EXTRA_REPLAY_DATA = "Replay Data";
 
     private static MyApplication sMyApplication;
 
@@ -85,8 +66,8 @@ public class MyApplication extends Application {
                 return mWebSocketClient = openNewConnection();
             }
         } else {
-            Intent intent = new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_NO_INTERNET_CONNECTION);
-            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+            BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                    BroadcastSender.EXTRA_NO_INTERNET_CONNECTION);
             return null;
         }
     }
@@ -144,7 +125,8 @@ public class MyApplication extends Application {
             try {
                 webSocketClient.send(message);
             } catch (WebsocketNotConnectedException e) {
-                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_NO_INTERNET_CONNECTION));
+                BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                        BroadcastSender.EXTRA_NO_INTERNET_CONNECTION);
             }
         }
     }
@@ -232,13 +214,15 @@ public class MyApplication extends Application {
                         onboarding.setSignedIn(true);
                         onboarding.setAvatar(avatar);
 
-                        LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_LOGIN_SUCCESSFUL).putExtra(EXTRA_LOGIN_SUCCESSFUL, username));
+                        BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                                BroadcastSender.EXTRA_LOGIN_SUCCESSFUL, username);
                     }
                     break;
                 case "nametaken":
                     channel = -1;
                     final String errorMessage = messageDetail.substring(messageDetail.indexOf('|') + 1);
-                    LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_ERROR_MESSAGE).putExtra(EXTRA_ERROR_MESSAGE, errorMessage));
+                    BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                            BroadcastSender.EXTRA_ERROR_MESSAGE, errorMessage);
                     break;
                 case "queryresponse":
                     channel = -1;
@@ -260,7 +244,8 @@ public class MyApplication extends Application {
                             break;
                         case "savereplay":
                             final String replayData = messageDetail.substring(messageDetail.indexOf('|') + 1);
-                            LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_REPLAY_DATA).putExtra(EXTRA_REPLAY_DATA, replayData));
+                            BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                                    BroadcastSender.EXTRA_REPLAY_DATA, replayData);
                             break;
                         default:
                             Log.d(MTAG, message);
@@ -273,12 +258,14 @@ public class MyApplication extends Application {
                 case "popup":
                     channel = -1;
                     final String popupMessage = messageDetail.substring(messageDetail.indexOf('|') + 1);
-                    LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_ERROR_MESSAGE).putExtra(EXTRA_ERROR_MESSAGE, popupMessage));
+                    BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                            BroadcastSender.EXTRA_ERROR_MESSAGE, popupMessage);
                     break;
                 case "updatesearch":
                     channel = -1;
                     final String searchStatus = messageDetail.substring(messageDetail.indexOf('|') + 1);
-                    LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_UPDATE_SEARCH).putExtra(EXTRA_UPDATE_SEARCH, searchStatus));
+                    BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                            BroadcastSender.EXTRA_UPDATE_SEARCH, searchStatus);
                     break;
                 case "pm":
                 case "usercount":
@@ -294,7 +281,10 @@ public class MyApplication extends Application {
         }
 
         if (channel == 1) {
-            LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_SERVER_MESSAGE).putExtra(EXTRA_SERVER_MESSAGE, message).putExtra(EXTRA_CHANNEL, channel).putExtra(EXTRA_ROOMID, "lobby"));
+            BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                    BroadcastSender.EXTRA_SERVER_MESSAGE, message,
+                    BroadcastSender.EXTRA_CHANNEL, Integer.toString(channel),
+                    BroadcastSender.EXTRA_ROOMID, "lobby");
         }
 
     }
@@ -316,9 +306,14 @@ public class MyApplication extends Application {
         }
 
         if (message.startsWith("init") && channel == 0) {
-            LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_NEW_BATTLE_ROOM).putExtra(EXTRA_ROOMID, roomId));
+            BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                    BroadcastSender.EXTRA_NEW_BATTLE_ROOM, null,
+                    BroadcastSender.EXTRA_ROOMID, roomId);
         } else {
-            LocalBroadcastManager.getInstance(MyApplication.this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_SERVER_MESSAGE).putExtra(EXTRA_SERVER_MESSAGE, message).putExtra(EXTRA_CHANNEL, channel).putExtra(EXTRA_ROOMID, roomId));
+            BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                    BroadcastSender.EXTRA_SERVER_MESSAGE, message,
+                    BroadcastSender.EXTRA_CHANNEL, Integer.toString(channel),
+                    BroadcastSender.EXTRA_ROOMID, roomId);
         }
     }
 
@@ -327,10 +322,12 @@ public class MyApplication extends Application {
         if (!onboarding.isSignedIn()) {
             if (onboarding.getKeyId() == null || onboarding.getChallenge() == null) {
                 getWebSocketClient();
-                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_NO_INTERNET_CONNECTION));
+                BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                        BroadcastSender.EXTRA_NO_INTERNET_CONNECTION);
                 return false;
             }
-            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ACTION_FROM_MY_APPLICATION).putExtra(EXTRA_DETAILS, EXTRA_REQUIRE_SIGN_IN));
+            BroadcastSender.get(this).sendBroadcastFromMyApplication(
+                    BroadcastSender.EXTRA_REQUIRE_SIGN_IN);
             return false;
         }
         return true;
