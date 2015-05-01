bc17c0efc7b8514cfcb45af504fef238a35d4626
Lennart Frick <lennart.frick@gmail.com>
2015-05-01 18:33:48 +0200

    More requested changes.

--- app/src/main/java/com/pokemonshowdown/data/ItemDex.java
+++ app/src/main/java/com/pokemonshowdown/data/ItemDex.java
@@ -1,26 +1,24 @@
 package com.pokemonshowdown.data;
 
 import android.content.Context;
-import android.util.Log;
 
-import com.paypal.android.sdk.D;
 import com.pokemonshowdown.app.R;
 import com.pokemonshowdown.application.MyApplication;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ItemDex {
     private final static String ITAG = ItemDex.class.getName();
-    public final static JSONObject DUMMY_OBJECT;
+    public final static JSONObject DUMMY_JSON_ITEM;
+    public final static String DUMMY_ITEM = "Item not loadable.";
 
     static {
         try {
-            DUMMY_OBJECT = new JSONObject("{\"gen\":1,\"spritenum\":0,\"num\":0,\"name\":\"\",\"id\":\"dummy\",\"desc\":\"\"}");
+            DUMMY_JSON_ITEM = new JSONObject("{\"gen\":1,\"spritenum\":0,\"num\":0,\"name\":\"Broken Item ID\",\"id\":\"dummy\",\"desc\":\"This item should not be displayed here. Please report this bug.\"}");
         } catch (JSONException e) {
             // Will not happen, but is needed because it's final.
             throw new Error();
@@ -28,9 +26,12 @@ public class ItemDex {
     }
 
     public static int getItemIcon(Context appContext, String itemName) {
-        itemName = itemName.replaceFirst("itemdex_", "");
-        return appContext.getResources()
-                .getIdentifier("item_" + MyApplication.toId(itemName), "drawable", appContext.getPackageName());
+        if (itemName != null) {
+            return appContext.getResources()
+                    .getIdentifier("item_" + MyApplication.toId(itemName), "drawable", appContext.getPackageName());
+        } else {
+            return R.drawable.sprites_0;
+        }
     }
 
     public static List<String> getItemDexEntries(Context context) {
@@ -38,30 +39,34 @@ public class ItemDex {
     }
 
     public static String getItemName(Context context, String name) {
-        name = MyApplication.toId(name);
-        String item;
-        try {
-            name = name.startsWith("itemDex_") ? name : "itemdex_" + name;
-            int stringId = context.getResources().getIdentifier(name, "string", context.getPackageName());
-                    item = context.getResources().getString(stringId);
-            if (item != null) {
-                JSONObject itemEntries = new JSONObject(item);
-                item = itemEntries.getString("name");
+        if (name != null) {
+            name = MyApplication.toId(name);
+            String itemName;
+            try {
+                int stringId = context.getResources().getIdentifier(name, "string", context.getPackageName());
+                itemName = context.getResources().getString(stringId);
+                JSONObject itemEntries = new JSONObject(itemName);
+                return itemEntries.getString("name");
+            } catch (JSONException e) {
+                //Handled in return statement the same way as an null name.
             }
-        } catch (JSONException e) {
-            item = null;
         }
-        return item;
+        return DUMMY_ITEM;
     }
 
     public static JSONObject getItemJsonObject(Context context, String name) {
         try {
-            int stringId = context.getResources().getIdentifier(name, "string", context.getPackageName());
-            String item = context.getString(stringId);
-            return new JSONObject(item);
-        } catch (NullPointerException | JSONException e) {
-            e.printStackTrace();
-            return DUMMY_OBJECT;
+            name = MyApplication.toId(name);
+            if(name != null) {
+                int stringId = context.getResources().getIdentifier(name, "string", context.getPackageName());
+                if (stringId > 0) {
+                    String item = context.getString(stringId);
+                    return new JSONObject(item);
+                }
+            }
+        } catch (JSONException e) {
+            //Handled in return statement
         }
+        return DUMMY_JSON_ITEM;
     }
 }
